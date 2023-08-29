bdacf89f0e40de7574206b9b84a31a71f7841b00
MortenMink <bubbor123123@gmail.com>
2023-08-29 09:23:21 +0200

    Fjernet en linje af digtet

--- Printer.py
+++ Printer.py
@@ -1,3 +1,3 @@
-print(" Skoven kræver sin virkelighed \n i mennesket (dets mumlen) \n det er dets friheds filosof \n en neksus har en ligevægt." )
+print(" Skoven kræver sin virkelighed \n det er dets friheds filosof \n en neksus har en ligevægt." )
 
 print(" Der findes af adskillige ting \n matematikken skriver drejer bløder")
